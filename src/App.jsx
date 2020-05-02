bd3b0cc867abcf501ae7e749711ecefc940e97df
Mahboob Monna Mohamed <mahboob@Mahboobs-MacBook-Pro.local>
2020-05-02 15:10:34 +0530

    added minimal layout with routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import React, { Component } from "react";
+import { createBrowserHistory } from "history";
+import { Router } from "react-router-dom";
+import Routes from "./Routes";
 
+const browserHistory = createBrowserHistory();
 export class App extends Component {
   render() {
-    return `App`;
+    return (
+      <Router history={browserHistory}>
+        <Routes />
+      </Router>
+    );
   }
 }
-
-export default App;
--- src/Routes.js
+++ src/Routes.js
@@ -1,9 +1,21 @@
-import React from 'react';
-import { Switch, Redirect} from 'react-router-dom';
-
+import React from "react";
+import { Switch, Redirect } from "react-router-dom";
+import RouteWithLayout from "./components/RouteWithLayout/RouteWithLayout";
+import { NotFound } from "./views";
+import { Minimal } from "./layouts";
 
 const Routes = () => {
-    return (
-         
-    )
-}
+  return (
+    <Switch>
+      <Redirect exact from="/" to="/not-found" />
+      <RouteWithLayout
+        component={NotFound}
+        path="/not-found"
+        layout={Minimal}
+        exact
+      />
+    </Switch>
+  );
+};
+
+export default Routes;

5c77a1fc1c4d27584478da7047ffdd2a9d368bce
Mahboob Monna Mohamed <mahboob@Mahboobs-MacBook-Pro.local>
2020-05-02 14:51:10 +0530

    added two layout files

---
+++ src/Routes.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Switch, Redirect} from 'react-router-dom';
+
+
+const Routes = () => {
+    return (
+         
+    )
+}
---
+++ src/layouts/Minimal/Minimal.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Minimal = (props) => {
+  return `Minimal`;
+};
+
+export default Minimal;

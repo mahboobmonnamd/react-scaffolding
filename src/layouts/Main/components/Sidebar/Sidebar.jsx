9c4580b0d62c7dcd50ea68f747371d8a92abb323
Mahboob Monna Mohamed <mahboob@Mahboobs-MacBook-Pro.local>
2020-05-03 14:51:13 +0530

    updated main layout

---
+++ src/layouts/Main/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Sidebar = (props) => {
+  return <div>Sidebar</div>;
+};
+
+export default Sidebar;
---
+++ src/layouts/Main/components/Topbar/Topbar.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Topbar = (props) => {
+  return <div>Top bar</div>;
+};
+
+export default Topbar;
